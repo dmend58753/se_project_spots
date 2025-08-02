9d095ab48af19346fec41620550b104421e005c6
Daniel Mendoza <dmendoza11597@gmail.com>
2025-08-02 15:57:22 -0400

    project 8

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,8 @@ const initialCards = [
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("Script is running");
+
   const popupProfile = document.querySelector("#edit-profile-modal");
   const popupAddCard = document.querySelector("#new-post-modal");
 
@@ -38,7 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const profileNameEl = document.querySelector(".profile__name");
   const profileDescriptionEl = document.querySelector(".profile__description");
 
-  const cardsContainer = document.querySelector(".cards__list");
+
+  const previewModal = document.querySelector("#preview-image-modal");
+  const previewImage = previewModal.querySelector(".modal__image");
+  const previewCaption = previewModal.querySelector(".modal__caption");
+
+ const cardsContainer = document.querySelector(".cards__list");
+
 
   function openModal(modal) {
     modal.style.visibility = "visible";
@@ -48,6 +56,52 @@ document.addEventListener("DOMContentLoaded", () => {
   function closeModal(modal) {
     modal.style.visibility = "hidden";
     modal.classList.remove("modal_is-opened");
+  }
+
+  const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".card");
+  const cardsList = document.querySelector(".cards__list");
+
+  function getCardElement(data) {
+    console.log("card, data.name");
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardTitleEl = cardElement.querySelector(".card__title");
+    const cardImageEl = cardElement.querySelector(".card__image");
+
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.name;
+    cardTitleEl.textContent = data.name;
+
+cardImageEl.addEventListener("click", () => {
+  previewImage.src = data.link;
+  previewImage.alt = data.name;
+  previewCaption.textContent = data.name;
+  openModal(previewModal);
+});
+
+
+
+//like button logic
+const likeButtonEl = cardElement.querySelector(".card__like-button");
+const likeIconEl = likeButtonEl.querySelector("img");
+
+  likeButtonEl.addEventListener("click", () => {
+    likeButtonEl.classList.toggle("card__like-button_active");
+      if (likeButtonEl.classList.contains("card__like-button_active")) {
+    likeIconEl.src = "./spots-images/Liked-button.svg";
+  } else {
+    likeIconEl.src = "./spots-images/Like-Icon.svg";
+  }
+  });               
+
+ // delete button logic
+  const deleteButtonEl = cardElement.querySelector(".card__delete-button");
+  deleteButtonEl.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+    return cardElement;
   }
 
   editButton.addEventListener("click", () => {
@@ -78,6 +132,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   profileEditForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log("Profile updated");
     const nameInput = popupProfile.querySelector("#profile-name-input");
     const descriptionInput = popupProfile.querySelector(
       "#profile-description-input"
@@ -86,4 +141,30 @@ document.addEventListener("DOMContentLoaded", () => {
     profileDescriptionEl.textContent = descriptionInput.value;
     closeModal(popupProfile);
   });
+
+  cardForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const imageInput = popupAddCard.querySelector("#card-image-input");
+    const descInput = popupAddCard.querySelector("#post-description-input");
+
+    const inputValues = {
+      name: descInput.value,
+      link: imageInput.value,
+    };
+
+    const cardElement = getCardElement(inputValues);
+    cardsList.prepend(cardElement);
+    closeModal(popupAddCard);
+  });
+
+  initialCards.forEach(function (item) {
+    const cardElement = getCardElement(item);
+    cardsList.append(cardElement);
+  });
+
+previewModal.addEventListener("mousedown", (e) => {
+  if (e.target === previewModal) {
+    closeModal(previewModal);
+  }
+});
 });
